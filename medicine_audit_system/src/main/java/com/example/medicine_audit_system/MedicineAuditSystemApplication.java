39fe3f3118d5912f3681fd3669dea5437512ba2d
poteto1212 <makt.itou.home1212@gmail.com>
2022-03-09 00:28:21 +0900

    ロジェクト成

---
+++ medicine_audit_system/src/main/java/com/example/medicine_audit_system/MedicineAuditSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.medicine_audit_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineAuditSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineAuditSystemApplication.class, args);
+	}
+
+}
---
+++ medicine_audit_system/src/test/java/com/example/medicine_audit_system/MedicineAuditSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicine_audit_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineAuditSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
