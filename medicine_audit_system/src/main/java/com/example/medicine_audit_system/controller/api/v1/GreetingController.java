7dac206d6c374b6607e8d0425d29f32ce9a42fdf
poteto1212 <makt.itou.home1212@gmail.com>
2022-03-10 09:34:20 +0900

    デバッグ中

--- medicine_audit_system/src/main/java/com/example/medicine_audit_system/controller/api/v1/GreetingController.java
+++ medicine_audit_system/src/main/java/com/example/medicine_audit_system/controller/api/v1/GreetingController.java
@@ -4,15 +4,21 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import com.example.medicine_audit_system.response.Greeting;
 @RestController
+@RequestMapping("/api/v1") 
 @CrossOrigin
 public class GreetingController {
     private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		Greeting test = new Greeting(1, "test", "testevv");
+		long id =test.getId();
+		test.getContent();
+
 		return new Greeting(counter.incrementAndGet(), String.format(template, name),"makoto");
 	}
 
