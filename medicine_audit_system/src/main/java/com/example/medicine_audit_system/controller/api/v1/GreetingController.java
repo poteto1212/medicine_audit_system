c88b5c4313e030e89db0e3bd42f5bcec3dc59bc0
poteto1212 <makt.itou.home1212@gmail.com>
2022-03-11 10:26:06 +0900

    エンドポイント変更

--- medicine_audit_system/src/main/java/com/example/medicine_audit_system/controller/api/v1/GreetingController.java
+++ medicine_audit_system/src/main/java/com/example/medicine_audit_system/controller/api/v1/GreetingController.java
@@ -4,6 +4,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import com.example.medicine_audit_system.response.Greeting;
 @RestController
