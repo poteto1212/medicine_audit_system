7cae647214c68c236a431b67855ea716208d6947
poteto1212 <makt.itou.home1212@gmail.com>
2022-03-30 16:35:40 +0900

    init

--- medicine_audit_system/src/main/java/com/example/medicine_audit_system/controller/api/v1/GreetingController.java
+++ medicine_audit_system/src/main/java/com/example/medicine_audit_system/controller/api/v1/GreetingController.java
@@ -21,9 +21,8 @@ public class GreetingController {
     @Autowired
 	TestuserService testuserService;
 	@GetMapping("/greeting")
-	
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name),"mako");
+		return new Greeting(counter.incrementAndGet(), String.format(template, name),"makoto");
 	}
 
 	@Autowired
