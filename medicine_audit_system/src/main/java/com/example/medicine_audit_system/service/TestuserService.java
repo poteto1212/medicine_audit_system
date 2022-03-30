d87ef67e8e7cbdbd0370f6235b15359dbd497acc
poteto1212 <makt.itou.home1212@gmail.com>
2022-03-30 16:23:59 +0900

    Spring Data Jpaの接続情報を環境変数化

--- medicine_audit_system/src/main/java/com/example/medicine_audit_system/service/TestuserService.java
+++ medicine_audit_system/src/main/java/com/example/medicine_audit_system/service/TestuserService.java
@@ -1,5 +1,4 @@
 package com.example.medicine_audit_system.service;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
