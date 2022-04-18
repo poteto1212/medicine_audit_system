cc65e8faed04cd5988e0093bda0ff8c79b93f62a
poteto1212 <makt.itou.home1212@gmail.com>
2022-04-18 11:06:52 +0900

    テスト用データベースに向き先変更

--- medicine_audit_system/src/test/java/com/example/medicine_audit_system/repository/TestuserRepositoryTest.java
+++ medicine_audit_system/src/test/java/com/example/medicine_audit_system/repository/TestuserRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
@@ -30,7 +31,7 @@ import com.example.medicine_audit_system.model.Testuser;
 @DbUnitConfiguration(
     dataSetLoader = ReplacementCsvDataSetLoader.class
 )
-
+@TestPropertySource(locations="/application-test.properties")
 @Transactional
 public class TestuserRepositoryTest{
     private static final String DATA_FILE_PATH = "/test_dataset/";
